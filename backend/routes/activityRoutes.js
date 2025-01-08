d3d6acc79e663e7fe93046989a29344d5d4d440c
Skanda Ganesh P V <49191239+Skandaganesh@users.noreply.github.com>
2025-01-08 20:05:00 +0530

    Delete backend directory

--- backend/routes/activityRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/activityRoutes.js
-const express = require('express');
-const router = express.Router();
-const activityController = require('../controllers/activityController');
-
-router.post('/activities', activityController.createActivity); // Log activity
-router.get('/activities/:id', activityController.getActivitiesByUserId); // Get activities by user ID
-
-module.exports = router;
--- backend/routes/recipeRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/recipeRoutes.js
-const express = require('express');
-const router = express.Router();
-const recipeController = require('../controllers/recipeController');
-
-router.post('/recipes', recipeController.createRecipe); // Create recipe
-router.get('/recipes', recipeController.getAllRecipes); // Get all recipes
-
-module.exports = router;
